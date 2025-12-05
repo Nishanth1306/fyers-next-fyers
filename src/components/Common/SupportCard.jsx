5b1f6112275ad8016b739149ce9b2d752e5338c5
Nishanth <nishanth.k@codingmart.com>
2025-12-05 18:14:55 +0530

    Fix - contact us page

--- src/components/Common/SupportCard.jsx
+++ src/components/Common/SupportCard.jsx
@@ -12,29 +12,32 @@ const iconMap = {
   checkmark: ApiSupport,
 };
 
-export const SupportCard = ({ card }) => {
+export const SupportCard = ({ card, isLast }) => {
   const IconComponent = iconMap[card.icon];
 
   return (
-    <div className="p-[24px] md:p-6 border-r border-b border-l border-gray-200 flex flex-row md:flex-col items-start gap-2 md:gap-[8px]">
-      
+    <div
+      className={`px-5 py-4 md:p-6 border-b border-l border-r ${
+        isLast ? "md:border-r" : "md:border-r-0"
+      } border-[#C8C8CC] flex flex-row items-start gap-2 md:gap-[8px]`}
+    >
       {/* Icon */}
-      <div className="flex-shrink-0 flex items-center justify-center bg-white border border-gray-200 w-[32px] h-[32px]">
+      <div className="flex-shrink-0 flex items-center justify-center bg-white border border-gray-200 w-[32px] h-[32px] py-[2.72px] px-[2.73px]">
         {IconComponent && <IconComponent className="w-4 h-4 text-gray-700" />}
       </div>
 
       {/* Content */}
-      <div className="flex-1 flex flex-col justify-start">
+      <div className="flex flex-col items-start gap-[4px] self-stretch max-w-[338px]">
         {/* Title */}
-        <h3 className="font-plus-jakarta text-[20px] md:text-[22px] font-semibold text-[#131319] leading-tight mb-2">
+        <h3 className="font-plus-jakarta text-[22px] font-semibold text-[#131319] leading-[32px] -tracking-[0.32px]">
           {card.title}
         </h3>
 
-        <div className="flex flex-col justify-between text-[#131319] font-manrope text-sm md:text-base gap-1">
+        <div className="text-[#131319] font-manrope text-sm md:text-base">
           {card.hours && <p>{card.hours}</p>}
           {card.subHours && <p>{card.subHours}</p>}
-          {card.phone && <p className="font-medium text-gray-900">{card.phone}</p>}
-          {card.email && <p className="font-medium text-gray-900">{card.email}</p>}
+          {card.phone && <p>{card.phone}</p>}
+          {card.email && <p>{card.email}</p>}
         </div>
       </div>
     </div>
