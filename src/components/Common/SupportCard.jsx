a1286345905716b8a26d9304a6e39402671403b6
Nishanth <nishanth.k@codingmart.com>
2025-12-05 17:57:02 +0530

    Fix - Support Card

--- src/components/Common/SupportCard.jsx
+++ src/components/Common/SupportCard.jsx
@@ -13,18 +13,18 @@ const iconMap = {
 };
 
 export const SupportCard = ({ card }) => {
-  const IconComponent = iconMap[card.icon]
+  const IconComponent = iconMap[card.icon];
 
   return (
-    <div className="px-5 py-4 md:p-6 border-r border-b border-l border-gray-200 flex flex-col md:flex-row items-start gap-2 md:gap-[8px]">
+    <div className="p-[24px] md:p-6 border-r border-b border-l border-gray-200 flex flex-row md:flex-col items-start gap-2 md:gap-[8px]">
+      
       {/* Icon */}
-      <div className="flex-shrink-0 flex items-center justify-center bg-white border border-gray-200 w-[32px] h-[32px] py-[2.72px] px-[2.73px]">
+      <div className="flex-shrink-0 flex items-center justify-center bg-white border border-gray-200 w-[32px] h-[32px]">
         {IconComponent && <IconComponent className="w-4 h-4 text-gray-700" />}
       </div>
 
-
       {/* Content */}
-      <div className="flex-1 mt-2 md:mt-0">
+      <div className="flex-1 flex flex-col justify-start">
         {/* Title */}
         <h3 className="font-plus-jakarta text-[20px] md:text-[22px] font-semibold text-[#131319] leading-tight mb-2">
           {card.title}
